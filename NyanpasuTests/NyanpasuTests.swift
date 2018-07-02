2dabe1b793ae87537ced8b168964bd5d9f6a7c8d
Yuske Fukuyama <yuskefuku@gmail.com>
2018-07-02 11:27:28 +0900

    init nyanpasu

---
+++ NyanpasuTests/NyanpasuTests.swift
@@ -0,0 +1,36 @@
+//
+//  NyanpasuTests.swift
+//  NyanpasuTests
+//
+//  Created by Yuske Fukuyama on 2018/07/01.
+//  Copyright © 2018 Yuske Fukuyama. All rights reserved.
+//
+
+import XCTest
+@testable import Nyanpasu
+
+class NyanpasuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
